051d676b1559365fd9ced237983fa5d384b50d30
taljacobson <tal1989@gmail.com>
2018-03-09 17:38:26 +0200

    move logic to its own widget

--- lib/ui/todo_list.dart
+++ lib/ui/todo_list.dart
@@ -1,59 +1,29 @@
 import 'package:flutter/material.dart';
 import '../utils/todos.dart';
+import './single_todo.dart';
 
 class TodoList extends StatelessWidget {
   final List<Todo> todos;
-  final ValueChanged<int> _onTap;
-  final ValueChanged<int> _onLongPress;
-  TodoList(this.todos, this._onTap, this._onLongPress);
+  final ValueChanged<int> onTap;
+  final ValueChanged<int> onLongPress;
+  TodoList({this.todos, this.onTap, this.onLongPress});
 
   @override
   Widget build(BuildContext context) {
     return new Flexible(
       child: new ListView.builder(
         itemCount: todos.length,
-        itemBuilder: (context, index) {
-          final message = todos[index];
-          return _todoItem(message, index);
-        },
-      ),
-    );
-  }
 
-  Widget _todoItem(Todo message, int index) {
-    return new InkWell(
-      onTap: () => _onTap(index),
-      onLongPress: () => _onLongPress(index),
-      child: new Container(
-        //modified
-        margin: const EdgeInsets.symmetric(vertical: 1.0),
-        child: new Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: new IconButton(
-                onPressed: () => _onTap(index),
-                icon: new Icon(message.completed
-                    ? Icons.check_box
-                    : Icons.check_box_outline_blank),
-                color:
-                    message.completed ? Colors.greenAccent : Colors.blueAccent,
-              ),
-            ),
-            new Expanded(
-              child: new Text(
-                message.todo,
-                style: new TextStyle(
-                  decoration: message.completed
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-            ),
-          ],
-        ),
+        
+        itemBuilder: (context, index) {
+          final todo = todos[index];
+          return new SingleTodo(
+            todo: todo,
+            onTap: onTap,
+            onLongPress: onLongPress,
+            index: index,
+          );
+        },
       ),
     );
   }
